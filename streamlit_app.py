b13aeaf3d6111ea3b350c537cfa288d01f3bec38
PARITALA PAVAN KUMAR <pavankumarparitala2580@gmail.com>
2026-04-01 11:41:28 +0530

    Change app title and description for clarity
    Updated the title and introductory text for the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Cloud Computing Lab Internal")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    " This is an Example web application to demonstarate in Cloud Computing Lab Internal.
+    Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
