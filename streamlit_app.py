eb289d4ad57cbe7c57be321aea998e9aebbfdbca
PARITALA PAVAN KUMAR <pavankumarparitala2580@gmail.com>
2026-04-01 11:47:09 +0530

    Fix typo in Streamlit app description
    Corrected a typo in the description text.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,6 @@ import streamlit as st
 st.title("🎈 Cloud Computing Lab Internal")
 st.write(
     " This is an Example web application to demonstarate in Cloud Computing Lab Internal.
+    Testing
     Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
